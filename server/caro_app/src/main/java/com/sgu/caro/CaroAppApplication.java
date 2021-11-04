26771fce633d6e0c428f921bfc81913e8eace163
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2021-11-04 23:06:13 +0700

    [Add]: Init project

---
+++ server/caro_app/src/main/java/com/sgu/caro/CaroAppApplication.java
@@ -0,0 +1,13 @@
+package com.sgu.caro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaroAppApplication.class, args);
+	}
+
+}
