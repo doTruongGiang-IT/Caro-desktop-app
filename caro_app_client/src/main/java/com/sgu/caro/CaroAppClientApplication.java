b58cf91201d9b1cd1eb7283d9ab347f9b853bba2
doTruongGiang-IT <dotruonggiang290@gmail.com>
2021-11-13 10:03:06 +0700

    [Add]: Init project client server

---
+++ caro_app_client/src/main/java/com/sgu/caro/CaroAppClientApplication.java
@@ -0,0 +1,13 @@
+package com.sgu.caro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaroAppClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaroAppClientApplication.class, args);
+	}
+
+}
