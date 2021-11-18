08f738d7fef190a275d049b4647f54a853b32863
Thien Nguyen <52367055+thien926@users.noreply.github.com>
2021-11-18 15:14:58 +0700

    [Modified]: Add Username and Score

--- caro_app_client/src/main/java/com/sgu/caro/GUI/MainScreen/LoadMatch.java
+++ caro_app_client/src/main/java/com/sgu/caro/GUI/MainScreen/LoadMatch.java
@@ -29,16 +29,21 @@ public class LoadMatch extends javax.swing.JFrame {
         
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        btnDongY.setEnabled(false);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
-
         mainPanel = new javax.swing.JPanel();
-        btlTime = new javax.swing.JLabel();
         lblClock = new javax.swing.JLabel();
         btnHuy = new javax.swing.JButton();
+        btnDongY = new javax.swing.JButton();
+        lblDoiThu = new javax.swing.JLabel();
+        lblDiem = new javax.swing.JLabel();
+        txtTime = new javax.swing.JLabel();
+        txtUserName = new javax.swing.JLabel();
+        txtScore = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -46,13 +51,9 @@ public class LoadMatch extends javax.swing.JFrame {
         mainPanel.setBackground(new java.awt.Color(204, 204, 255));
         mainPanel.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
-        btlTime.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        btlTime.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        btlTime.setText("30s");
-
         lblClock.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        lblClock.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblClock.setText("THỜI GIAN CHỜ");
+        lblClock.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        lblClock.setText("THỜI GIAN CHỜ:");
 
         btnHuy.setText("HỦY");
         btnHuy.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -61,26 +62,77 @@ public class LoadMatch extends javax.swing.JFrame {
             }
         });
 
+        btnDongY.setText("ĐỒNG Ý");
+        btnDongY.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnDongYMouseClicked(evt);
+            }
+        });
+
+        lblDoiThu.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        lblDoiThu.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        lblDoiThu.setText("Đối thủ:");
+
+        lblDiem.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        lblDiem.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        lblDiem.setText("Điểm:");
+
+        txtTime.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        txtTime.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        txtTime.setText("30s");
+
+        txtUserName.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        txtUserName.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        txtUserName.setText("???");
+
+        txtScore.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        txtScore.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        txtScore.setText("???");
+
         javax.swing.GroupLayout mainPanelLayout = new javax.swing.GroupLayout(mainPanel);
         mainPanel.setLayout(mainPanelLayout);
         mainPanelLayout.setHorizontalGroup(
             mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(mainPanelLayout.createSequentialGroup()
-                .addGap(111, 111, 111)
-                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnHuy, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lblClock, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btlTime, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(113, Short.MAX_VALUE))
+                .addGap(32, 32, 32)
+                .addComponent(btnHuy, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnDongY, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(26, 26, 26))
+            .addGroup(mainPanelLayout.createSequentialGroup()
+                .addGap(45, 45, 45)
+                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(lblDiem, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblDoiThu, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblClock, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(txtScore, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtUserName, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtTime, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(40, Short.MAX_VALUE))
         );
         mainPanelLayout.setVerticalGroup(
             mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(mainPanelLayout.createSequentialGroup()
-                .addComponent(lblClock, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap()
+                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(mainPanelLayout.createSequentialGroup()
+                        .addComponent(lblClock, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(lblDoiThu, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
+                        .addComponent(lblDiem, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(mainPanelLayout.createSequentialGroup()
+                        .addComponent(txtTime, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtUserName, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(txtScore, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(18, 18, 18)
-                .addComponent(btlTime, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btnHuy, javax.swing.GroupLayout.DEFAULT_SIZE, 53, Short.MAX_VALUE)
+                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnHuy, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnDongY, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap())
         );
 
@@ -105,11 +157,20 @@ public class LoadMatch extends javax.swing.JFrame {
         MainScreenDesign.loadMatch = true;
     }                                   
 
+    private void btnDongYMouseClicked(java.awt.event.MouseEvent evt) {                                      
+        // TODO add your handling code here:
+    }                                     
+
 
     // Variables declaration - do not modify                     
-    private javax.swing.JLabel btlTime;
+    private javax.swing.JButton btnDongY;
     private javax.swing.JButton btnHuy;
     private javax.swing.JLabel lblClock;
+    private javax.swing.JLabel lblDiem;
+    private javax.swing.JLabel lblDoiThu;
     private javax.swing.JPanel mainPanel;
+    private javax.swing.JLabel txtScore;
+    private javax.swing.JLabel txtTime;
+    private javax.swing.JLabel txtUserName;
     // End of variables declaration                   
 }
