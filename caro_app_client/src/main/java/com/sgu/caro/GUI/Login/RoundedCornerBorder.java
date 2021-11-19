158a54ceaa0d8a0640d7fbd4b1b47300d3aef782
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2021-11-19 22:20:37 +0700

    [Modified]: API call

--- caro_app_client/src/main/java/com/sgu/caro/GUI/Login/RoundedCornerBorder.java
+++ caro_app_client/src/main/java/com/sgu/caro/GUI/Login/RoundedCornerBorder.java
@@ -14,28 +14,33 @@ import java.awt.geom.RoundRectangle2D;
 import javax.swing.border.AbstractBorder;
 
 public class RoundedCornerBorder extends AbstractBorder {
-	  private static final Color ALPHA_ZERO = new Color(0x0, true);
-	  public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
-	    Graphics2D g2 = (Graphics2D) g.create();
-	    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-	    Shape border = getBorderShape(x, y, width - 1, height - 1);
-	    g2.setPaint(ALPHA_ZERO);
-	    Area corner = new Area(new Rectangle2D.Double(x, y, width, height));
-	    corner.subtract(new Area(border));
-	    g2.fill(corner);
-	    g2.setPaint(Color.GRAY);
-	    g2.draw(border);
-	    g2.dispose();
-	  }
-	  public Shape getBorderShape(int x, int y, int w, int h) {
-	    int r = h; //h / 2;
-	    return new RoundRectangle2D.Double(x, y, w, h, r, r);
-	  }
-	  public Insets getBorderInsets(Component c) {
-	    return new Insets(4, 8, 4, 8);
-	  }
-	  public Insets getBorderInsets(Component c, Insets insets) {
-	    insets.set(4, 8, 4, 8);
-	    return insets;
-	  }
+
+    private static final Color ALPHA_ZERO = new Color(0x0, true);
+
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        Shape border = getBorderShape(x, y, width - 1, height - 1);
+        g2.setPaint(ALPHA_ZERO);
+        Area corner = new Area(new Rectangle2D.Double(x, y, width, height));
+        corner.subtract(new Area(border));
+        g2.fill(corner);
+        g2.setPaint(Color.GRAY);
+        g2.draw(border);
+        g2.dispose();
+    }
+
+    public Shape getBorderShape(int x, int y, int w, int h) {
+        int r = h; //h / 2;
+        return new RoundRectangle2D.Double(x, y, w, h, r, r);
+    }
+
+    public Insets getBorderInsets(Component c) {
+        return new Insets(4, 8, 4, 8);
+    }
+
+    public Insets getBorderInsets(Component c, Insets insets) {
+        insets.set(4, 8, 4, 8);
+        return insets;
+    }
 }
--- caro_app_client/src/main/java/com/sgu/caro/GUI/MainScreen/MainScreenDesign.java
+++ caro_app_client/src/main/java/com/sgu/caro/GUI/MainScreen/MainScreenDesign.java
@@ -1,5 +1,7 @@
 package com.sgu.caro.GUI.MainScreen;
 
+import com.sgu.caro.api_connection.TokenManager;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
