66529f5f310043f820e87cdd253d3308ca729064
doTruongGiang-IT <dotruonggiang290@gmail.com>
2021-11-22 22:52:54 +0700

    [Modified]: token time

--- caro_app_server/src/main/java/com/sgu/caro/token/JwtService.java
+++ caro_app_server/src/main/java/com/sgu/caro/token/JwtService.java
@@ -17,7 +17,7 @@ public class JwtService {
 
 	public static final String USERNAME = "username";
 	public static final String SECRET_KEY = "11111111111111111111111111111111";
-	public static final int EXPIRE_TIME = 100;
+	public static final int EXPIRE_TIME = 720000;
 	
 	public String generateTokenLogin(String username) {
 	    String token = null;
