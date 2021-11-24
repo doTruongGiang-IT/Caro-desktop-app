45281307049ee31dbe7da8424b884f6b0585bd8e
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2021-11-24 23:17:16 +0700

    [Modified]: Local environment

--- caro_app_client/src/main/java/com/sgu/caro/api_connection/TokenManager.java
+++ caro_app_client/src/main/java/com/sgu/caro/api_connection/TokenManager.java
@@ -4,7 +4,7 @@ public class TokenManager {
 
     private static String jwt;
     private static int user_id;
-    private static final String HOST = "http://139.177.184.240:8080";
+    private static final String HOST = "http://localhost:8080";
 
     public static String getJwt() {
         return jwt;
