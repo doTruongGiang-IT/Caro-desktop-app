7033e327be124950c240a02cfec9abaec31a6996
doTruongGiang-IT <dotruonggiang290@gmail.com>
2021-12-13 17:33:37 +0700

    [Add]: regex check date

--- caro_app_server_api/src/main/java/com/sgu/caro/controller/UserController.java
+++ caro_app_server_api/src/main/java/com/sgu/caro/controller/UserController.java
@@ -154,7 +154,11 @@ public class UserController {
         String pass = user.getPassword();
         Pbkdf2PasswordEncoder pbkdf2PasswordEncoder = new Pbkdf2PasswordEncoder();
         String regex = "([0-9]{4})/([0-9]{2})/([0-9]{2})";
-        if (!dateString.matches(regex)) {
+        String dateRegex = "^((2000|2400|2800|(19|2[0-9](0[48]|[2468][048]|[13579][26])))/02/29)$" 
+        	      + "|^(((19|2[0-9])[0-9]{2})/02/(0[1-9]|1[0-9]|2[0-8]))$"
+        	      + "|^(((19|2[0-9])[0-9]{2})/(0[13578]|10|12)/(0[1-9]|[12][0-9]|3[01]))$" 
+        	      + "|^(((19|2[0-9])[0-9]{2})/(0[469]|11)/(0[1-9]|[12][0-9]|30))$";
+        if (!dateString.matches(dateRegex)) {
             user.setDayOfBirth(null);
         };
         if (!gender.equals("male") && !gender.equals("female") && !gender.equals("undefiend")) {
