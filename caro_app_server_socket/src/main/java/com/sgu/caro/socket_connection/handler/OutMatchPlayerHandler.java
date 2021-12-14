7d01da64a420b2e3e8a8ef1dc34e0d79c453b628
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2021-12-14 22:15:19 +0700

    [Modified]: at com.sgu.caro.socket_connection.handler.OutMatchPlayerHandler.run (OutMatchPlayerHandler.java:39)

--- caro_app_server_socket/src/main/java/com/sgu/caro/socket_connection/handler/OutMatchPlayerHandler.java
+++ caro_app_server_socket/src/main/java/com/sgu/caro/socket_connection/handler/OutMatchPlayerHandler.java
@@ -35,67 +35,68 @@ public class OutMatchPlayerHandler {
             String dataSend;
             int result;
             AcceptPairingHandler acceptPairingHandler = new AcceptPairingHandler();
+            if (group != null){
+                int user_id_1 = group.getUser_1();
+                int user_id_2 = group.getUser_2();
+                Socket socket_1 = userList.get(Integer.toString(user_id_1));
+                Socket socket_2 = userList.get(Integer.toString(user_id_2));
 
-            int user_id_1 = group.getUser_1();
-            int user_id_2 = group.getUser_2();
-            Socket socket_1 = userList.get(Integer.toString(user_id_1));
-            Socket socket_2 = userList.get(Integer.toString(user_id_2));
+                if (user_1 == user_id_1) {
+                    dataSend = dataSocket.exportResultMatch(user_id_2, null, null);
+                    result = user_id_2;
+                } else {
+                    dataSend = dataSocket.exportResultMatch(user_id_1, null, null);
+                    result = user_id_1;
+                }
 
-            if (user_1 == user_id_1) {
-                dataSend = dataSocket.exportResultMatch(user_id_2, null, null);
-                result = user_id_2;
-            } else {
-                dataSend = dataSocket.exportResultMatch(user_id_1, null, null);
-                result = user_id_1;
+                BufferedWriter out_socket_1 = new BufferedWriter(new OutputStreamWriter(socket_1.getOutputStream()));
+                out_socket_1.write(dataSend);
+                out_socket_1.newLine();
+                out_socket_1.flush();
+
+                BufferedWriter out_socket_2 = new BufferedWriter(new OutputStreamWriter(socket_2.getOutputStream()));
+                out_socket_2.write(dataSend);
+                out_socket_2.newLine();
+                out_socket_2.flush();
+
+                for (int watcher_id : group.getWatchers()) {
+                    Socket socket_client = userList.get(Integer.toString(watcher_id));
+                    BufferedWriter out_client = new BufferedWriter(new OutputStreamWriter(socket_client.getOutputStream()));
+                    out_client.write(dataSend);
+                    out_client.newLine();
+                    out_client.flush();
+                }
+
+                new AcceptPairingHandler().removeGroup(user_1);
+
+                group.setEnd_date(LocalDateTime.now());
+                DateTimeFormatter dateFormater = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+                DataAPI dataAPI = new DataAPI();
+
+                int result_type = 3;
+                String start_date = dateFormater.format(group.getStart_date());
+                String end_date = dateFormater.format(group.getEnd_date());
+
+                String requestData = dataAPI.exportMatchAPI(user_id_1, user_id_2, result, result_type, start_date, end_date);
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request;
+                try {
+                    request = HttpRequest.newBuilder()
+                            .uri(new URI(APIConnection.postMatchAPIURL))
+                            .headers("Content-Type", "application/json;charset=UTF-8")
+                            .POST(HttpRequest.BodyPublishers.ofString(requestData))
+                            .build();
+                    HttpResponse response;
+                    response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                    JSONObject responseData = dataAPI.importData(response.body().toString());
+                } catch (URISyntaxException ex) {
+                    Logger.getLogger(GoStepHandler.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(GoStepHandler.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                System.out.println("Create Match with" + requestData);
             }
-
-            BufferedWriter out_socket_1 = new BufferedWriter(new OutputStreamWriter(socket_1.getOutputStream()));
-            out_socket_1.write(dataSend);
-            out_socket_1.newLine();
-            out_socket_1.flush();
-
-            BufferedWriter out_socket_2 = new BufferedWriter(new OutputStreamWriter(socket_2.getOutputStream()));
-            out_socket_2.write(dataSend);
-            out_socket_2.newLine();
-            out_socket_2.flush();
-
-            for (int watcher_id : group.getWatchers()) {
-                Socket socket_client = userList.get(Integer.toString(watcher_id));
-                BufferedWriter out_client = new BufferedWriter(new OutputStreamWriter(socket_client.getOutputStream()));
-                out_client.write(dataSend);
-                out_client.newLine();
-                out_client.flush();
-            }
-
-            new AcceptPairingHandler().removeGroup(user_1);
-
-            group.setEnd_date(LocalDateTime.now());
-            DateTimeFormatter dateFormater = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-
-            DataAPI dataAPI = new DataAPI();
-            
-            int result_type = 3;
-            String start_date = dateFormater.format(group.getStart_date());
-            String end_date = dateFormater.format(group.getEnd_date());
-
-            String requestData = dataAPI.exportMatchAPI(user_id_1, user_id_2, result, result_type, start_date, end_date);
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request;
-            try {
-                request = HttpRequest.newBuilder()
-                        .uri(new URI(APIConnection.postMatchAPIURL))
-                        .headers("Content-Type", "application/json;charset=UTF-8")
-                        .POST(HttpRequest.BodyPublishers.ofString(requestData))
-                        .build();
-                HttpResponse response;
-                response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                JSONObject responseData = dataAPI.importData(response.body().toString());
-            } catch (URISyntaxException ex) {
-                Logger.getLogger(GoStepHandler.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(GoStepHandler.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            System.out.println("Create Match with" + requestData);
         } catch (IOException ex) {
             Logger.getLogger(OutMatchPlayerHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
