98e727999709f51cec1fa02b1eb2a6c9696b4cb7
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2021-12-16 00:53:39 +0700

    [Add]: Patch 6

--- caro_app_server_socket/src/main/java/com/sgu/caro/logging/Logging.java
+++ caro_app_server_socket/src/main/java/com/sgu/caro/logging/Logging.java
@@ -11,32 +11,40 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 public class Logging {
+
     private static final String HOST = "https://log-api.newrelic.com/log/v1?Api-Key=ca38b594e69c0a8339bbdc10fee93560FFFFNRAL";
     public static final String SOCKET_TYPE = "SOCKET";
     public static final String MATCH_TYPE = "MATCH";
     private static final String OPTION = "LOGGING";
-    
-    public static void log(String message_type, String action_type, String message){
-        if (OPTION.equals("LOGGING")){
-            try {
-                JSONObject loggingObj = new JSONObject();
-                loggingObj.put("message_type", message_type);
-                loggingObj.put("action_type", action_type);
-                loggingObj.put("message", message);
-                HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(MediaType.APPLICATION_JSON);
-                HttpEntity<String> request = new HttpEntity<>(loggingObj.toString(), headers);
-                RestTemplate restTemplate = new RestTemplate();
-                String resultRestTemplate = restTemplate.postForObject(HOST, request, String.class);
-            } catch (final HttpClientErrorException httpClientErrorException) {
-                System.out.println(httpClientErrorException.getResponseBodyAsString());
-            } catch (HttpServerErrorException httpServerErrorException) {
-                System.out.println(httpServerErrorException);
-            } catch (JSONException | RestClientException exception) {
-                System.out.println(exception);
-            }
-        }
-        else{
+
+    public static void log(String message_type, String action_type, String message) {
+        if (OPTION.equals("LOGGING")) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    try {
+                        JSONObject loggingObj = new JSONObject();
+                        loggingObj.put("message_type", message_type);
+                        loggingObj.put("action_type", action_type);
+                        loggingObj.put("message", message);
+                        HttpHeaders headers = new HttpHeaders();
+                        headers.setContentType(MediaType.APPLICATION_JSON);
+                        HttpEntity<String> request = new HttpEntity<>(loggingObj.toString(), headers);
+                        RestTemplate restTemplate = new RestTemplate();
+                        String resultRestTemplate = restTemplate.postForObject(HOST, request, String.class);
+                    } catch (final HttpClientErrorException httpClientErrorException) {
+                        System.out.println(httpClientErrorException.getResponseBodyAsString());
+                    } catch (HttpServerErrorException httpServerErrorException) {
+                        System.out.println(httpServerErrorException);
+                    } catch (JSONException | RestClientException exception) {
+                        System.out.println(exception);
+                    }
+
+                }
+            });
+            thread.start();
+        } else {
             System.out.println("[" + message_type + "]" + "[" + action_type + "]: " + message);
         }
     }
